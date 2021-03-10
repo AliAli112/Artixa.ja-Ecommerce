67cb233d3ef86c4b3f0c1cefaf1d093468609956
Teriq Strachan <teriqstrachan@yahoo.com>
2021-03-10 00:01:36 -0500

    Basics of the order collator

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { BrowserRouter as Router, Switch, Route, Link,  useHistory, IndexRoute } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, useHistory, IndexRoute } from 'react-router-dom';
 import { RouteEntries } from './Domain Model/Routes';
 import { RegisterPage } from './UI/Register';
 import { AccountsPage } from './UI/Accounts'
@@ -13,26 +13,26 @@ import { OrderCollartorPage } from './UI/OrderCollartor'
 
 class App extends Component {
 
-  render () {
+  render() {
     return (
       <div className="App">
         <Router>
           <div>
             <Switch>
-              <Route exact path = {/*Should change this to register later or item catalog*/RouteEntries.index} component={ RegisterPage}>
+              <Route exact path={/*Should change this to register later or item catalog*/RouteEntries.index} component={OrderCollartorPage}>
                 {/* <AccountsPage /> */}
               </Route>
             </Switch>
           </div>
-          <Route path = {RouteEntries.accounts}>
+          <Route path={RouteEntries.accounts}>
             {/* <AccountsPage />  */}
           </Route>
-            <h1>Home</h1>
+          <h1>Home</h1>
         </Router>
       </div>
     );
   }
-  
+
 }
 
 export default App;
--- reactapp/src/UI/OrderCollartor.jsx
+++ reactapp/src/UI/OrderCollartor.jsx
@@ -1,57 +1,69 @@
 import React, { Component } from 'react'
 import { Item } from '../Domain Model/Item'
 import { Order } from '../Domain Model/Orders'
-
 //import { json } from '../json/data';
 import axios from 'axios'
 
 const server = axios.create()
 
-export class OrderCollartorPage extends Component{
-    list = [{name: 'meme', age: 40}, {name: 'me', age: 4}]
+export class OrderCollartorPage extends Component {
+
     state = {
-        orders: [],
-        
+        list: [{ name: 'meme', age: 40 }, { name: 'me', age: 4 }],
+        orders: []
+
 
     }
-    constructor(){
+    constructor() {
         super()
         this.getOrders()
     }
 
     addOrders = async () => {
         let res = await server.post('http://localhost:3005/orders', {
-        items: JSON.stringify(this.list),
-        shippingLocation: 'space',
-        }).then((res) =>{
+            items: JSON.stringify(this.list),
+            shippingLocation: 'space',
+        }).then((res) => {
             console.log(res)
             this.getExpenses();
         })
     }
-    
-    getOrders = async () => {
-        try{
-            let data = await server.get('http://localhost:3005/orders')
-        .then(({data}) => data)
-        var orders = [];
-        console.log(data)
-        
 
-        // for(let i = 0; i < data.lenght; i++){
-        //     let order = new
-        // }
-        console.log(JSON.parse(data[0].items))
-        }catch(err){
+    getOrders = async () => {
+        try {
+            let data = await server.get('http://localhost:3005/orders')
+                .then(({ data }) => data)
+            var orders = [];
+            console.log(data)
+
+
+            // for(let i = 0; i < data.lenght; i++){
+            //     let order = new
+            // }
+            console.log(JSON.parse(data[0].items))
+        } catch (err) {
             console.log(err)
         }
-        
+
     }
 
     render() {
-        return(
+        return (
             <div>
-                <h2>OrderCollatorPage</h2>
-                 <button onClick={this.addOrders}>Add</button>
+                <span id="order-col-head">
+                    <h2 className="active order-head">Outstanding Order</h2>
+                    <h2 className="order-head">Finished Orders</h2>
+                </span>
+
+                {this.state.list.map(item => (
+                    <div className="col-orders">
+                        <input type="checkbox" />
+                        <h3>{item["name"]}</h3>
+                        <h3>Location</h3>
+                        <img src="https://www.pinclipart.com/picdir/big/130-1304123_drop-down-arrow-svg-png-icon-free-download.png" alt="Drop Down"></img>
+                    </div>
+                ))}
+                <button onClick={this.addOrders}>Add</button>
             </div>
         )
     }
